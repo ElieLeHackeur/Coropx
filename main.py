4bd7bb7daedf702f6c5d46625195abbbf8a4cd1b
ElieLeHackeur <79536652+ElieLeHackeur@users.noreply.github.com>
2021-06-21 14:44:13 +0200

    main.py
    main.py [type de fichier] [taille de fichier en octet]

---
+++ main.py
@@ -0,0 +1,28 @@
+from sys import argv
+from os.path import isfile
+from os import rename
+
+caractere = "caca"
+ecriture = ""
+
+if len(argv) < 3:
+    print("\nErreur : Pas assez d'arguments \nExemple : nomdufichier.py pdf 500 \nCette commande va creer un fichier .pdf corrompu de 500 octets\n")
+    exit()
+
+if len(argv) > 3:
+    print("\nErreur : Trop assez d'arguments \nExemple : nomdufichier.py pdf 500 \nCette commande va creer un fichier .pdf corrompu de 500 octets\n")
+    exit()
+
+type_de_fichier = argv[1]
+taille_du_fichier = argv[2]
+
+for i in range(int(taille_du_fichier)):
+    ecriture += caractere
+    
+with open('fichier.txt', "w") as f :
+    f.write(ecriture)
+    f.close()
+
+rename('fichier.txt', f'fichier.{type_de_fichier}')
+
+print("le fichier a été créé")
